43a3cacdc520988b9568136b6e442a9322359932
Koen Aerts <koen.aerts@pm.me>
2021-04-22 23:58:07 +0200

    geo plotting

---
+++ scripts/geo.py
@@ -0,0 +1,25 @@
+import json
+
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+from scripts.utils import get_amount_of_cookies
+
+data = [['France', np.mean(get_amount_of_cookies('france').values)],
+        ['Belgium', np.mean(get_amount_of_cookies('belgium').values)],
+        ['Netherlands', np.mean(get_amount_of_cookies('the_netherlands').values)]]
+
+df = pd.DataFrame(data, columns=['Country', 'Number of cookies mean'])
+
+with open('./geo/custom.geo.json') as response:
+    countries = json.load(response)
+
+fig = px.choropleth(df, geojson=countries, color="Number of cookies mean",
+                    locations="Country", featureidkey="properties.admin",
+                    projection="mercator", color_continuous_scale=px.colors.sequential.Viridis
+                    )
+fig.update_geos(fitbounds="locations", scope="europe", visible=False, showcountries=True, resolution=110,
+                countrycolor="Black")
+fig.update_layout(margin={"r": 0, "t": 200, "l": 0, "b": 200}, legend=dict(orientation="h"))
+fig.show()
